b906fed77a9d0738849db30a8b052d28db9d6cca
Alexiboi <alexhanna413@gmail.com>
2026-09-21 11:00:29 +0100

    Change theme of frontpage to match daily bugle logog (black/red/white)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import ArticleCard from './components/ArticleCard'
 import OutletPicker from './components/OutletPicker'
 import Pagination from './components/Pagination'
 import SearchBar from './components/SearchBar'
+import logo from './assets/daily_bugle_logo_trimmed.png'
 import './App.css'
 
 const PAGE_SIZE = 10
@@ -23,6 +24,7 @@ function App() {
     setStatus('loading')
     setError('')
     try {
+      console.log(keyword)
       const results = await searchArticles(keyword, { domains })
 
       setArticles(results)
@@ -41,7 +43,10 @@ function App() {
 
   return (
     <main className="app">
-      <h1>News Search</h1>
+      <header className="masthead">
+        <img className="masthead-logo" src={logo} alt="" />
+        <h1>The Daily Bugle</h1>
+      </header>
       <SearchBar onSearch={handleSearch} disabled={status === 'loading'} allowEmpty={domains.length > 0} />
       <OutletPicker selected={domains} onChange={setDomains} />
 
